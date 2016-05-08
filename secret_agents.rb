c4a083b8a0cd9181c76374a45acb2327ef1093ca
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-07 21:54:01 -0700

    final commit

--- secret_agents.rb
+++ secret_agents.rb
@@ -1,43 +1,60 @@
-# ask user for input
-
-puts "Please enter a random word:"
-random_word = gets.chomp
-
-#define encrypt method
-
+# ask for user input
+puts "Please enter your password:"
+$random_word = gets.chomp
+# write encryption method
 def encrypt(word)
  index = 0
-
+# skips encrypting spaces
  while index < word.length
+  if word[index] == " "
+  word[index] = " "
+# fixes edge cases
+  elsif word[index] == "z"
+  word[index] = "a"  
+# encrypt to the next letters
+  else  
   word[index] = word[index].next!
-  index += 1
  end
+ index += 1
+ end
+# output encryption
  puts "#{word}"
+ return word
 end
-
-#define encrypt method
-
+# create decryption method
 def decrypt(word)
  index = 0
  alphabet = "abcdefghijklmnopqrstuvwxyz"
  while index < word.length
+  if word[index] == " "
+  word[index] = " "
+# go backwards in alphabet one letter
+  else
   letter = alphabet.index(word[index])
   word[index] = alphabet[(letter-1)]
+  end
   index += 1
  end
+# output decryption to user
  puts "#{word}"
 end
 
-# ask user what they would like to do to the input
+# ask user if they want decrypted or encrypted
 
-puts "Would you like to encrypt or decrypt it?"
-choice = gets.chomp
+def inquiry
+ puts "Would you like to encrypt or decrypt it?"
+ choice = gets.chomp
 
-if choice == "encrypt"
- encrypt(random_word)
-elsif choice == "decrypt"
- decrypt(random_word)
-else 
- puts "I didn't quite catch that... try again"
- start
+ if choice == "encrypt"
+  encrypt($random_word)
+ elsif choice == "decrypt"
+  decrypt($random_word)
+ elsif choice == "both"
+  decrypt(encrypt($random_word))
+  else 
+  puts "I didn't quite catch that... try again"
+  inquiry
+ end
 end
+
+inquiry
