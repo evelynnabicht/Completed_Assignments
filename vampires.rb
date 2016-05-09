4b9fe1da0ecae52142b47ac28bc7dd77662c4a7b
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-09 08:29:00 -0700

    added in more code

---
+++ vampires.rb
@@ -0,0 +1,48 @@
+puts "How many employees will be processed?"
+employees = gets.chomp
+
+employees.times do 
+  
+
+
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+input = gets.chomp
+
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+order = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance?"
+insurance = gets.chomp
+
+if 
+  age_right && (likes_garlic || wants_insurance)
+
+  puts "Probably not a vampire."
+
+elsif 
+  age_wrong && (no_garlic || no_insurance)
+
+  puts "Probably a vampire" 
+
+elsif
+  age_wrong && no_garlic && no_insurance
+
+  puts "Almost certainly a vampire."
+
+elsif
+  name = "Drake Cula" || "Tu Fang"
+
+  puts "definitely a vampire"
+
+else
+  puts "Results Inconclusive"
+
+
+  end
