1047e8732d24faa3479696bb8ea7570e41d8b731
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-10 20:08:29 -0700

    initial commit

--- method_detective.rb
+++ method_detective.rb
@@ -1,39 +1,42 @@
 # Replace in the "<???>" with the appropriate method (and arguments, if any).
 # Uncomment the calls to catch these methods red-handed.
 
+
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
-# => “InVeStIgAtIoN”
+"iNvEsTiGaTiOn".swapcase
+ => “InVeStIgAtIoN”
 
-# "zom".<???>
-# => “zoom”
+"zom".insert(2, 'o')
+ => “zoom”
 
-# "enhance".<???>
-# => "    enhance    "
+"enhance".center(15)
+=> "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
-# => "STOP! YOU’RE UNDER ARREST!"
+"Stop! You’re under arrest!".upcase
+=> "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
-#=> "the usual suspects"
+ "the usual".insert(8, 'suspects')
+=> "the usual suspects"
 
-# " suspects".<???>
-# => "the usual suspects"
+" suspects".insert(-9, 'the usual ')
+ => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
-# => "The case of the disappearing last lette"
+ "The case of the disappearing last letter".chop
+ => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
-# => "he mystery of the missing first letter"
+ "The mystery of the missing first letter".delete "T"
+ => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
-# => "Elementary, my dear Watson!"
+ "Elementary,    my   dear        Watson!".strip
+ => "Elementary, my dear Watson!"
 
-# "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+ "z".ord
+ => 122 
+# (What is the significance of the number 122 in relation to the character z?
+#  - Z is the ASCII character for the number 122 )
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count('a')
+=> 4
+
