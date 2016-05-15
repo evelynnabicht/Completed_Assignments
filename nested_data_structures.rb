b4c5b2d9583939862397d7020d294354ece95ef3
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-15 16:21:10 -0700

    final commit

--- nested_data_structures.rb
+++ nested_data_structures.rb
@@ -1,10 +1,10 @@
-mall = {
+shopping_mall = {
   forever_21: {
     real_name: 'Forever 21',
     top_selling_items: {
       "Tops" => "Flower Blouse",
-      "Bottoms" => "Leather Pants"
-      "Shoes" => "Chunky Heels"
+      "Bottoms" => "Leather Pants",
+      "Shoes" => "Chunky Heels",
       "Accessories" => "Cat Earrings"
     },
     style: 
@@ -15,9 +15,9 @@ mall = {
   abercrombie_fitch: {
     real_name: 'Abercrombie and Fitch',
     top_selling_items: {
-      "Tops" => "Cropped Tank-Top"
-      "Bottoms" => "Skinny Jeans"
-      "Shoes" => "Flip-Flops"
+      "Tops" => "Cropped Tank-Top",
+      "Bottoms" => "Skinny Jeans",
+      "Shoes" => "Flip-Flops",
       "Accessories" => "Women's Perfume"
     },
     style: 
@@ -28,9 +28,9 @@ mall = {
 pac_sun: {
   real_name: 'Pac Sun',
   top_selling_items: {
-    "Tops" => "Ripped Jacket"
-    "Bottoms" => "Jeggings"
-    "Shoes" => "Vans"
+    "Tops" => "Ripped Jacket",
+    "Bottoms" => "Jeggings",
+    "Shoes" => "Vans",
     "Accessories" => "Skateboard Trucks"
   },
   style: 
@@ -39,3 +39,22 @@ pac_sun: {
   },
 }
 
+puts "What is Forever 21's style?"
+p shopping_mall[:forever_21][:style]
+puts ""
+
+puts "Please delete the current style for Pac Sun"
+shopping_mall[:pac_sun][:style].delete("Clothing for skater kids who don't care about mainstream culture")
+puts ""
+
+puts "Please add a new style for Pac Sun"
+shopping_mall[:pac_sun][:style].push("California-inspired apparel")
+puts ""
+
+puts "Your new style for Pac Sun is:"
+p shopping_mall[:pac_sun][:style]
+puts ""
+
+puts "what is the first thing listed under Abercrombie and Fitch's top selling items?"
+p shopping_mall[:abercrombie_fitch][:top_selling_items].first
+puts ""
