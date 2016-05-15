4e8ba7da0d5bd64216dad82d3bc2539c7278bcb2
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-05-15 15:57:00 -0700

    added stores

---
+++ nested_data_structures.rb
@@ -0,0 +1,41 @@
+mall = {
+  forever_21: {
+    real_name: 'Forever 21',
+    top_selling_items: {
+      "Tops" => "Flower Blouse",
+      "Bottoms" => "Leather Pants"
+      "Shoes" => "Chunky Heels"
+      "Accessories" => "Cat Earrings"
+    },
+    style: 
+      ["Trendy young people clothes"]
+      
+
+  },
+  abercrombie_fitch: {
+    real_name: 'Abercrombie and Fitch',
+    top_selling_items: {
+      "Tops" => "Cropped Tank-Top"
+      "Bottoms" => "Skinny Jeans"
+      "Shoes" => "Flip-Flops"
+      "Accessories" => "Women's Perfume"
+    },
+    style: 
+      ["Overpriced preppy clothing for teenagers"]
+    
+
+  },
+pac_sun: {
+  real_name: 'Pac Sun',
+  top_selling_items: {
+    "Tops" => "Ripped Jacket"
+    "Bottoms" => "Jeggings"
+    "Shoes" => "Vans"
+    "Accessories" => "Skateboard Trucks"
+  },
+  style: 
+    ["Clothing for skater kids who don't care about mainstream culture"]
+    
+  },
+}
+
