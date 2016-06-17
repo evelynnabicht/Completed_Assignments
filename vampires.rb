b9297d3d5490d24d85bc5610322f7db6081c0666
Evelyn Nabicht <evelyn.nabicht@gmail.com>
2016-06-17 15:33:17 -0700

    final commit

--- vampires.rb
+++ vampires.rb
@@ -1,57 +1,64 @@
-puts "How many employees will be processed?"
-employees = gets.chomp.to_i
-num_cycles = 0;
+#Creating a survey for vampire employees
 
-while num_loops < employees
+require 'date'
+current_year = Date.today.year
+current_year = current_year.to_i
 
+count = 0
+puts "How many employees will you be enrolling?"
+num_of_employees = gets.chomp.to_i
 
+while count < num_of_employees
+puts ""
+
+#Retrieving data
 
 puts "What is your name?"
-name = gets.chomp
+name = gets.chomp.split(" ").map(&:capitalize).join(" ")
 
 puts "How old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "What year were you born?"
-input = gets.chomp
-
+year = gets.chomp.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-order = gets.chomp
+garlic_bread = gets.chomp
 
 puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
+health_insurance = gets.chomp
 
-allergy = ""
-    while allergy != "done" && allergy != "sunshine"
-        puts "Enter allergy then press enter (type 'done' when finished)"
-        allergy = gets.chomp
-    end
- 
- if allergy == "sunshine"
-        puts "Probably a vampire"
-    else
-    case 
-    
-    if age.to_i > 100 && (wants_garlic == "yes" || wants_health_insurance == "yes")
-            puts "Probably not a vampire."
-    
-    elsif age.to_i < 100 && (wants_garlic == "no" || wants_health_insurance == "no")
-            puts "Probably a vampire."
-    
-    elsif age.to_i < 100 && wants_garlic == "no" && wants_health_insurance == "no"
-            puts "Almost certainly a vampire."
-    
-    elsif name == "Drake Cula" || name == "Tu Fang"
-            
-            puts "Definitely a vampire."
-        
-        else
-            puts "Results inconclusive."
-        
-        end
-    end
+#Testing Data
 
-    num_cycles += 1
+if age == current_year - year && (garlic_bread == "yes" || health_insurance == "yes")
+    puts "Probably not a vampire."
+  elsif age != current_year - year && (garlic_bread == "no" || health_insurance == "no")
+    puts "Probably a vampire."
+  elsif age != current_year - year && (garlic_bread == "no" && health_insurance == "no")
+    puts "Almost certainly a vampire"
+  elsif name == "Drake Cula" || name == "Tu Fang"
+    puts "Definitely a vampire"
+  else
+    puts "Results inconclusive"
 end
-puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+#checking for allergies
+
+allergies = "sunshine"
+user_input = " "
+puts "Please list all your allergies and write 'Done' when you are done listing."
+while user_input != "sunshine"
+user_input = gets.chomp.downcase
+break if user_input == "done"
+end
+
+if user_input == "sunshine"
+  puts "Probably a vampire."
+end
+
+count += 1
+end
+
+#Ending
+
+puts "Actually, nevermind! What do these questions have to do with anything? Let's all be friends."
